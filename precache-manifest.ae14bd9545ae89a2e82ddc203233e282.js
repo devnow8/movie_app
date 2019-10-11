1c1eb2f617b16e2f514c8c31ffe8168f6e8b8c78
devnow8 <72devdev@gmail.com>
2019-10-11 20:43:42 +0900

    Updates

---
+++ precache-manifest.ae14bd9545ae89a2e82ddc203233e282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f302bf69da267d1fcb0fa4734ff95ee",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "415d696b950856ffc88a",
+    "url": "/movie_app/static/css/main.5083c271.chunk.css"
+  },
+  {
+    "revision": "881ec82cedbad11c4c4c",
+    "url": "/movie_app/static/js/2.75465cbe.chunk.js"
+  },
+  {
+    "revision": "415d696b950856ffc88a",
+    "url": "/movie_app/static/js/main.9a5ca02e.chunk.js"
+  },
+  {
+    "revision": "d72445ade3fc7a4cdf4d",
+    "url": "/movie_app/static/js/runtime-main.b4758674.js"
+  }
+]);
